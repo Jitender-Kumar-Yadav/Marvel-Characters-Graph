c957deff85bf54c41d4e8158b954e304d563adf8
Jitender <jkyadav1006@gmail.com>
2021-01-04 11:56:11 +0530

    Added the function to read the edges and modify the adjacency list of the graph.

--- MarvelGraph.java
+++ MarvelGraph.java
@@ -8,12 +8,18 @@ import java.util.ArrayList;
 public class MarvelGraph{
 	public static void main(String[] args) throws Exception{
 		Graph Marvel = new Graph();
-		CSVReader.nodesRead(args[0], Marvel);
+		CSVReader.nodesRead(args[0], Marvel);    //read the file containing vertices and store them
+		CSVReader.edgesRead(args[1], Marvel);    //read the file containing edges and create an adjacency list
+		System.out.println(Marvel.size + " " + Marvel.edgesize);
 	}
 }
 
 class CSVReader{
 	static void nodesRead(String name, Graph G) throws Exception{
+		//This function reads the file containing vertices and numbers them while updating the list of vertices
+		//Time Complexity:: O(n); n = number of vertices
+		//Space Complexity:: O(n); n = number of vertices
+		
 		String id, label, temp;    //temporary dynamic variables
 		Vertex v;    //pointer for vertex
 		G.indices = new HashMap<String, Integer>();
@@ -24,11 +30,13 @@ class CSVReader{
 		read.next();
 		read.next();    //ignore the headers, i.e. id and label
 		while(read.hasNext()){
+			//reading the id
 			id = read.next();
 			if(id.charAt(0) == '"'){
 				temp = read.next();
 				id = id.substring(1) + "," + temp.substring(0, temp.length() - 2);
 			}
+			//reading the label
 			label = read.next();
 			if(label.charAt(0) == '"'){
 				temp = read.next();
@@ -41,14 +49,57 @@ class CSVReader{
 		read.close();
 		G.size = count;    //modify the total number of nodes in the graph
 	}
+	
+	static void edgesRead(String name, Graph G) throws Exception{
+		//This function reads the file containing edges (co-occurences)
+		//and updates the adjacency list
+		//Time Complexity:: O(m); m = number of edges
+		//Space Complexity:: O(m); m = number of edges
+		
+		String source, target, temp;
+		Vertex v1, v2;
+		int weight, count;
+		G.adjList = new ArrayList<LinkedList<Edge>>();
+		for(int i = 0; i < G.size; i++){G.adjList.add(new LinkedList<Edge>());}    //initialise the adjacency list
+		Scanner read = new Scanner(new File(name));    //define a new scanner object to read edges' file
+		read.useDelimiter(",|\\n");    //Separate the file by using , or \n as separators
+		read.next();
+		read.next();
+		read.next();    //ignore the headers, i.e. source, target and weight
+		count = 0;
+		while(read.hasNext()){
+			//reading the source label
+			source = read.next();
+			if(source.charAt(0) == '"'){
+				temp = read.next();
+				source = source.substring(1) + "," + temp.substring(0, temp.length() - 2);
+			}
+			//reading the target label
+			target = read.next();
+			if(target.charAt(0) == '"'){
+				temp = read.next();
+				target = target.substring(1) + "," + temp.substring(0, temp.length() - 2);
+			}
+			//reading the weight
+			weight = Integer.parseInt(read.next());
+			v1 = G.vertices.get(G.indices.get(source));    //store the source vertex
+			v2 = G.vertices.get(G.indices.get(target));    //store the target vertex
+			//create two edges and parse these in the adjacency list of v1 and v2
+			G.adjList.get(G.indices.get(source)).add(new Edge(v1, v2, weight));
+			G.adjList.get(G.indices.get(target)).add(new Edge(v2, v1, weight));
+			count ++;
+		}
+		read.close();
+		G.edgesize = count;
+	}
 }
 
 class Graph{
 	//this class stores the main graph
-	int size;    //integer to store number of vertices in the graph
+	int size, edgesize;    //integer to store number of vertices and edges in the graph
 	HashMap<String, Integer> indices;    //this hashmap stores the index for each label of a vertex
 	ArrayList<Vertex> vertices;    //this list stores the vertices at correct index
-	LinkedList<Edge>[] adjList;    //the list shall store the linked lists containing the adjacenct list of each vertex
+	ArrayList<LinkedList<Edge>> adjList;    //the list shall store the linked lists containing the adjacenct list of each vertex
 	void Graph(){}
 }
 
@@ -66,10 +117,12 @@ class Vertex{
 
 class Edge{
 	//objects of this class are edges of the main Graph
-	Vertex[] end;    //stores the integers representing end point vertices
+	//these shall be stored in adjacency list
+	Vertex[] end;    //stores the vertices representing end points of the edge
 	//Since the graph is undirected, the ordering shall not matter
+	//nonetheless the source shall be the current vertex in whose adjacency list the edge is added
 	int weight;    //stores the weight of each edge
-	void Edge(Vertex source, Vertex target, int weight){
+	Edge(Vertex source, Vertex target, int weight){
 		end = new Vertex[2];
 		end[0] = source;
 		end[1] = target;
